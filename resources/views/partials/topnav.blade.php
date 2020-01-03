3ba78c1b38f7070aaa400facc69d640cf0093abc
naufalrandi <naufalrandialdiansyah@gmail.com>
2020-01-03 21:28:03 +0700

    Update layout login

--- resources/views/partials/topnav.blade.php
+++ resources/views/partials/topnav.blade.php
@@ -16,7 +16,9 @@
                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
         {{ __('Logout') }}
       </a>
-
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+        </form>
     </div>
   </li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
@@ -19,6 +20,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UserController');
